7fa602d4bc42341dc8c504593201d5fe117cb967
artFetisov <fetishfestoff@mail.ru>
2023-07-27 19:52:25 +0300

    cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { ValidationPipe } from "./pipes/validation.pipe";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { CorsMiddleware } from "./middlewares/cors.middleware";
+import { corsMiddleware } from "./middlewares/cors.middleware";
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.use(CorsMiddleware);
+  app.use(corsMiddleware);
 
   app.useGlobalPipes(new ValidationPipe());
 
--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,12 +1,8 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
-@Injectable()
-export class CorsMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  }
+export function corsMiddleware(req: Request, res: Response, next: NextFunction) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 }
