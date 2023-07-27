2fb573dbb706f79543f02cb48c9cd13e6f2e2fc7
artFetisov <fetishfestoff@mail.ru>
2023-07-27 19:41:43 +0300

    cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from "./pipes/validation.pipe";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { CorsMiddleware } from "./middlewares/cors.middleware";
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // app.use(cookieParser());
+  app.use(CorsMiddleware);
 
   app.useGlobalPipes(new ValidationPipe());
 
