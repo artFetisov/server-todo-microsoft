9be7cfe4c9249d1b6a1ab79aeec656353eafbe9f
artFetisov <fetishfestoff@mail.ru>
2023-07-28 00:20:33 +0300

    cors middleware

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 export function corsMiddleware(req: Request, res: Response, next: NextFunction) {
+  res.statusCode = 200;
   res.header("Access-Control-Allow-Credentials", "*");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
